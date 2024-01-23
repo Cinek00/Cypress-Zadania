3a9b211164254c9619043f09a34f8c2e8ab11769
Cinek00 <130596129+Cinek00@users.noreply.github.com>
2024-01-23 22:51:59 +0100

    Aktualizacja-komend

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,30 +1,5 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
 //////////////////////////////////////////////////////////////
+
     // komenda pod zadanie moduł 2 [Login i password LMS GoIT]
     //Test1
 Cypress.Commands.add("login1", (login, password) => {
@@ -36,6 +11,5 @@ Cypress.Commands.add("login2", (login, password) => {
   cy.get("#user_email").type(login);
   cy.get("#user_password").type(password);
 });
-
 
 //////////////////////////////////////////////////////////////
