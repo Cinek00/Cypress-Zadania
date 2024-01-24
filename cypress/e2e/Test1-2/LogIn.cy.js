b9f50dbd66b0892c7187ac6c6eb58b0f7a91a9f9
Cinek00 <130596129+Cinek00@users.noreply.github.com>
2024-01-24 18:15:49 +0100

    poprawki-komend-LogIN

---
+++ cypress/e2e/Test1-2/LogIn.cy.js
@@ -0,0 +1,22 @@
+/// <reference types= "cypress" />
+
+describe("login test do LMS goIT", () => {
+  // Test 1
+    it("Test1 login", () => {
+      cy.visit("https://www.edu.goit.global/account/login");
+       cy.login("user888@gmail.com", "1234567890");
+       cy.get('.eckniwg2').click();
+       cy.get('#open-navigation-menu-mobile').click();
+       cy.get(':nth-child(11) > .next-bve2vl').click();
+    });
+  // Test 2
+    it("Test2 login", () => {
+      cy.visit("https://www.edu.goit.global/account/login");
+       cy.login("testowyqa@qa.team", "QA!automation-1");
+       cy.get('.eckniwg2').click();
+       cy.get('#open-navigation-menu-mobile').click();
+       cy.get(':nth-child(9) > .next-bve2vl').click();
+    });
+  });
+
+  
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,14 +2,8 @@
 
     // komenda pod zadanie moduł 2 [Login i password LMS GoIT]
     //Test1
-Cypress.Commands.add("login1", (login, password) => {
+Cypress.Commands.add("login", (login, password) => {
     cy.get("#user_email").type(login);
     cy.get("#user_password").type(password);
   });
-//Test 2
-Cypress.Commands.add("login2", (login, password) => {
-  cy.get("#user_email").type(login);
-  cy.get("#user_password").type(password);
-});
-
 //////////////////////////////////////////////////////////////
